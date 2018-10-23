335cd2fedcb83481ca82e9dc1e01eafa181fbe9f
Boncheol Gu <boncheol.gu@gmail.com>
2018-10-23 22:13:03 +0900

    Initial commit

---
+++ src/dummy.cpp
@@ -0,0 +1,5 @@
+#include "tensorflow/core/platform/default/integral_types.h"
+
+namespace util {
+tensorflow::uint64 Fingerprint64(char const *, unsigned long) { return 0; }
+} // namespace util
---
+++ src/main.cpp
@@ -0,0 +1,64 @@
+#include <cstdio>
+
+#include "tensorflow/contrib/lite/interpreter.h"
+#include "tensorflow/contrib/lite/kernels/register.h"
+#include "tensorflow/contrib/lite/model.h"
+#include "tensorflow/contrib/lite/optional_debug_tools.h"
+
+// https://github.com/tensorflow/tensorflow/blob/master/tensorflow/contrib/lite/examples/minimal/minimal.cc
+
+// This is an example that is minimal to read a model
+// from disk and perform inference. There is no data being loaded
+// that is up to you to add as a user.
+//
+// NOTE: Do not add any dependencies to this that cannot be built with
+// the minimal makefile. This example must remain trivial to build with
+// the minimal build tool.
+//
+// Usage: minimal <tflite model>
+
+using namespace tflite;
+
+#define TFLITE_MINIMAL_CHECK(x)                                                \
+  if (!(x)) {                                                                  \
+    fprintf(stderr, "Error at %s:%d\n", __FILE__, __LINE__);                   \
+    exit(1);                                                                   \
+  }
+
+int main(int argc, char *argv[]) {
+  if (argc != 2) {
+    fprintf(stderr, "minimal <tflite model>\n");
+    return 1;
+  }
+  const char *filename = argv[1];
+
+  // Load model
+  std::unique_ptr<tflite::FlatBufferModel> model =
+      tflite::FlatBufferModel::BuildFromFile(filename);
+  TFLITE_MINIMAL_CHECK(model != nullptr);
+
+  // Build the interpreter
+  tflite::ops::builtin::BuiltinOpResolver resolver;
+  InterpreterBuilder builder(*model.get(), resolver);
+  std::unique_ptr<Interpreter> interpreter;
+  builder(&interpreter);
+  TFLITE_MINIMAL_CHECK(interpreter != nullptr);
+
+  // Allocate tensor buffers.
+  TFLITE_MINIMAL_CHECK(interpreter->AllocateTensors() == kTfLiteOk);
+  printf("=== Pre-invoke Interpreter State ===\n");
+  tflite::PrintInterpreterState(interpreter.get());
+
+  // Fill input buffers
+  // TODO(user): Insert code to fill input tensors
+
+  // Run inference
+  TFLITE_MINIMAL_CHECK(interpreter->Invoke() == kTfLiteOk);
+  printf("\n\n=== Post-invoke Interpreter State ===\n");
+  tflite::PrintInterpreterState(interpreter.get());
+
+  // Read output buffers
+  // TODO(user): Insert getting data out code.
+
+  return 0;
+}
