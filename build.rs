047e39e8564d3805a785071830f44a409730f90e
Boncheol Gu <boncheol.gu@gmail.com>
2018-10-26 03:11:55 +0900

    Use tensorflow releases instead git-submodule

--- build.rs
+++ build.rs
@@ -1,33 +1,115 @@
 extern crate bindgen;
 extern crate cpp_build;
+extern crate curl;
+extern crate failure;
+extern crate flate2;
+extern crate hex;
+extern crate reqwest;
+extern crate sha2;
+extern crate tar;
 
 use std::env;
+use std::fs;
+use std::path::{Path, PathBuf};
+use std::process::Command;
 
-fn build_inline_cpp() {
-    println!(
-        "cargo:rustc-link-search=native={}/external/tensorflow/tensorflow/contrib/lite/tools/make/gen/linux_x86_64/lib", env::var("CARGO_MANIFEST_DIR").unwrap()
+use failure::Fallible;
 
-    );
-    println!("cargo:rustc-link-lib=static=tensorflow-lite");
-    println!("cargo:rustc-link-lib=dylib=pthread");
-    println!("cargo:rustc-link-lib=dylib=dl");
+const TFLITE_VERSION: &'static str = "1.11.0";
 
-    cpp_build::Config::new()
-        .include("external/tensorflow")
-        .include(
-            "external/tensorflow/tensorflow/contrib/lite/tools/make/downloads/flatbuffers/include",
-        )
-        .flag("-fPIC")
-        .flag("-std=c++11")
-        .flag("-Wno-sign-compare")
-        .define("GEMMLOWP_ALLOW_SLOW_SCALAR_FALLBACK", None)
-        .debug(true)
-        .opt_level(2)
-        .build("src/lib.rs");
+fn is_valid_tf_src<P: AsRef<Path>>(filepath: P) -> bool {
+    use sha2::{Digest, Sha256};
+
+    let mut sha256 = Sha256::new();
+    sha256.input(&fs::read(filepath).unwrap());
+    ::hex::encode(sha256.result())
+        == "f49ce3f1d04cee854bc9f74fa9696991140b34a2e2447f35f01391b72c8bfa9f"
+}
+
+fn download<P: reqwest::IntoUrl, P2: AsRef<Path>>(source_url: P, target_file: P2) -> Fallible<()> {
+    let mut resp = reqwest::get(source_url)?;
+    let f = fs::File::create(&target_file)?;
+    let mut writer = ::std::io::BufWriter::new(f);
+    resp.copy_to(&mut writer)?;
+    Ok(())
+}
+
+fn extract<P: AsRef<Path>, P2: AsRef<Path>>(archive_path: P, extract_to: P2) {
+    use flate2::read::GzDecoder;
+    use tar::Archive;
+
+    let file = fs::File::open(archive_path).unwrap();
+    let unzipped = GzDecoder::new(file);
+    let mut a = Archive::new(unzipped);
+    a.unpack(extract_to).unwrap();
+}
+
+fn prepare_tensorflow_source() -> PathBuf {
+    let tf_src_dir = PathBuf::from(env::var("OUT_DIR").unwrap()).join("tensorflow");
+    fs::create_dir(&tf_src_dir).unwrap_or_default();
+
+    let tf_src_name = tf_src_dir.join(format!("v{}.tar.gz", TFLITE_VERSION));
+    if !tf_src_name.exists() || !is_valid_tf_src(&tf_src_name) {
+        let tf_src_url = format!(
+            "https://codeload.github.com/tensorflow/tensorflow/tar.gz/v{}",
+            TFLITE_VERSION
+        );
+
+        download(&tf_src_url, &tf_src_name).unwrap();
+
+        assert!(
+            is_valid_tf_src(&tf_src_name),
+            "{} is not valid",
+            tf_src_name.to_str().unwrap()
+        );
+    }
+
+    let tf_src_dir_inner = tf_src_dir.join(format!("tensorflow-{}", TFLITE_VERSION));
+    if !tf_src_dir_inner.exists() {
+        extract(&tf_src_name, &tf_src_dir);
+
+        Command::new("tensorflow/contrib/lite/tools/make/download_dependencies.sh")
+            .current_dir(&tf_src_dir_inner)
+            .status()
+            .expect("failed to download tflite dependencies.");
+    }
+    tf_src_dir_inner
+}
+
+fn prepare_tensorflow_library<P: AsRef<Path>>(tflite: P) {
+    let tf_lib_name = PathBuf::from(env::var("OUT_DIR").unwrap()).join("libtensorflow-lite.a");
+    if !tf_lib_name.exists() {
+        if download(&format!(
+            "https://raw.githubusercontent.com/boncheolgu/asset/master/library/tensorflow/v{VERSION}/{OS}_{ARCH}/libtensorflow-lite.a",
+            VERSION=TFLITE_VERSION,
+            OS=std::env::consts::OS,
+            ARCH=std::env::consts::ARCH,
+        ), &tf_lib_name).is_err() {
+            // if download failed, build it
+            Command::new("make")
+                .arg("-j")
+                .arg("-f")
+                .arg("tensorflow/contrib/lite/tools/make/Makefile")
+                .current_dir(&tflite)
+                .status()
+                .expect("failed to download tflite dependencies.");
+
+            fs::copy(
+                tflite
+                    .as_ref()
+                    .join(format!(
+                        "tensorflow/contrib/lite/tools/make/gen/{OS}_{ARCH}/lib/libtensorflow-lite.a",
+                        OS=std::env::consts::OS,
+                        ARCH=std::env::consts::ARCH,
+                    )),
+                &tf_lib_name,
+            ).unwrap();
+        }
+    }
 }
 
 // This generates "tflite_types.rs" containing structs and enums which are inter-operable with Glow.
-fn import_tflite_types() {
+fn import_tflite_types<P: AsRef<Path>>(tflite: P) {
     use bindgen::*;
 
     let bindings = bindgen::Builder::default()
@@ -51,8 +133,11 @@ fn import_tflite_types() {
         .blacklist_type("tflite::Interpreter_State")
         .default_enum_style(bindgen::EnumVariation::Rust)
         .header("csrc/tflite_wrapper.hpp")
-        .clang_arg("-Iexternal/tensorflow")
-        .clang_arg("-Iexternal/tensorflow/tensorflow/contrib/lite/tools/make/downloads/flatbuffers/include")
+        .clang_arg(format!("-I{}", tflite.as_ref().to_str().unwrap()))
+        .clang_arg(format!(
+            "-I{}/tensorflow/contrib/lite/tools/make/downloads/flatbuffers/include",
+            tflite.as_ref().to_str().unwrap()
+        ))
         .clang_arg("-DGEMMLOWP_ALLOW_SLOW_SCALAR_FALLBACK")
         .clang_arg("-x")
         .clang_arg("c++")
@@ -61,13 +146,36 @@ fn import_tflite_types() {
     let bindings = bindings.generate().expect("Unable to generate bindings");
 
     // Write the bindings to the $OUT_DIR/tflite_types.rs file.
-    let out_path = std::path::PathBuf::from(env::var("OUT_DIR").unwrap()).join("tflite_types.rs");
+    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap()).join("tflite_types.rs");
     bindings
         .write_to_file(out_path)
         .expect("Couldn't write bindings!");
 }
 
+fn build_inline_cpp<P: AsRef<Path>>(tflite: P) {
+    println!("cargo:rustc-link-lib=static=tensorflow-lite");
+    println!("cargo:rustc-link-lib=dylib=pthread");
+    println!("cargo:rustc-link-lib=dylib=dl");
+
+    cpp_build::Config::new()
+        .include(tflite.as_ref())
+        .include(
+            tflite
+                .as_ref()
+                .join("tensorflow/contrib/lite/tools/make/downloads/flatbuffers/include"),
+        )
+        .flag("-fPIC")
+        .flag("-std=c++11")
+        .flag("-Wno-sign-compare")
+        .define("GEMMLOWP_ALLOW_SLOW_SCALAR_FALLBACK", None)
+        .debug(true)
+        .opt_level(2)
+        .build("src/lib.rs");
+}
+
 fn main() {
-    import_tflite_types();
-    build_inline_cpp();
+    let tflite_src_dir = prepare_tensorflow_source();
+    prepare_tensorflow_library(&tflite_src_dir);
+    import_tflite_types(&tflite_src_dir);
+    build_inline_cpp(&tflite_src_dir);
 }
