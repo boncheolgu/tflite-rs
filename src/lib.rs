853e8df7f61c26cca3f037be61d4b44a3deea723
Tyler Hawkes <tyler.hawkes@vivint.com>
2019-05-15 10:28:53 -0600

    Debugging tests

--- src/lib.rs
+++ src/lib.rs
@@ -13,17 +13,3 @@ pub mod ops;
 
 pub use interpreter::Interpreter;
 pub use model::{FlatBufferModel, InterpreterBuilder};
-
-#[test]
-#[should_panic]
-fn threadsafe_types() {
-    fn send_sync<T: Send + Sync>(_t: &T) {}
-    let model = FlatBufferModel::default();
-    send_sync(&model);
-    let resolver = ops::builtin::resolver::Resolver::default();
-    send_sync(&resolver);
-    let builder = InterpreterBuilder::<'static>::new(&model, &resolver).expect("Not able to build builder");
-    send_sync(&builder);
-    let interpreter = builder.build().expect("Not able to build model");
-    send_sync(&interpreter);
-}
--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -75,3 +75,16 @@ fn mobilenetv2_mnist() {
     f.read_to_end(&mut buf).unwrap();
     test_mnist(&FlatBufferModel::build_from_buffer(&buf).unwrap()).unwrap();
 }
+
+#[test]
+fn threadsafe_types() {
+    fn send_sync<T: Send + Sync>(_t: &T) {}
+    let model = FlatBufferModel::build_from_file("data/MNISTnet_uint8_quant.tflite").expect("Unable to build flatbuffer model");
+    send_sync(&model);
+    let resolver = BuiltinOpResolver::default();
+    send_sync(&resolver);
+    let builder = InterpreterBuilder::new(&model, &resolver).expect("Not able to build builder");
+    send_sync(&builder);
+    let interpreter = builder.build().expect("Not able to build model");
+    send_sync(&interpreter);
+}
